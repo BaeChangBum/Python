4db86fa7dfe4e6aae5dc4dd0e8050d371edc1e11
BaeChangBum <changbum10001@gmail.com>
2022-08-28 23:38:13 +0900

    Hangman #2

--- Python 부트캠프/7. 행맨/Hangman.py
+++ Python 부트캠프/7. 행맨/Hangman.py
@@ -1,8 +1,9 @@
-word_list = ["ardvark", "baboon", "camel"]
-
-from pickle import FALSE
 import random
-
+from hanman_words import word_list
+  
+from hanman_art import logo
+print(logo)
+ 
 chosse_word = random.choice(word_list)
 print(f'Pssst, the solution is {chosse_word}')
 
@@ -10,12 +11,29 @@ guess_list = []
 for letter in range(len(chosse_word)):
     guess_list.append('_')
 print(guess_list)
+ 
+lives = 6
+end_of_game = False
 
-guess = input("Guess a letter: ").lower()
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
+        
+    for nPosition in range(len(chosse_word)):
+        letter = chosse_word[nPosition]
+        if letter == guess:
+            guess_list[nPosition] = guess
     
-for nPosition in range(len(chosse_word)):
-    letter = chosse_word[nPosition]
-    if letter == guess:
-        guess_list[nPosition] = guess
+    if guess not in chosse_word:
+        lives -= 1
+        if lives == 0:
+            end_of_game = True
+            print("You lose.")
 
-print(guess_list)
+    print(guess_list)
+    
+    if "_" not in guess_list:
+        end_of_game = True
+        print("You Win")
+        
+    from hanman_art import stages 
+    print(stages[lives])
